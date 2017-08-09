a4527a9cf57cd31eb937cb5be63bb297c01f2a87
Andrew Coulton <andrew@ingenerator.com>
2017-08-09 17:49:53 +0100

    Update build dependencies and build with Chef 12 & 13
    Fix simple foodcritic warnings, add a matrix build covering 12 & 13
    and update the build tooling to latest supported versions.
    
    Don't clear cookbooks between tests to speed up rspec
    
    Ignore build-time files from uploaded chef cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
 name 'ingenerator-source'
 maintainer 'Andrew Coulton'
 maintainer_email 'andrew@ingenerator.com'
-license 'Apache 2.0'
+license 'Apache-2.0'
+chef_version '>=12.18.31'
 description 'Manages deployment and provisioning of project source code on an instance, either live or VM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://github.com/ingenerator/chef-ingenerator-source/issues'
